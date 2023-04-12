ed5e4d2bed89001907e1f7105bdd7fb4ced1dbc6
Aidin <aidin.forooghi@gmail.com>
2023-04-11 22:54:54 -0400

    Passed 1 Test

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', () => {
+        // Do something when button is clicked
+    });
 }
